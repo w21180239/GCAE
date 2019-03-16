5d93dd6bea8f90003841d2891bf049373157b6e7
Liu_Wei <w21180239@163.com>
2019-03-16 21:45:33 +0800

    Bagging && GRU  78.92 -lr 5e-3 -batch-size 128 -verbose 1 -model CNN_Gate_Aspect -embed_file glove -r_l r -year 14 -epochs 6 -matrix_size 1000 -decoder_num 5 -gpu_id 0 -l2 0

--- model_files/cnn_gate_aspect_model.py
+++ model_files/cnn_gate_aspect_model.py
@@ -37,9 +37,9 @@ class CNN_Gate_Aspect_Text(nn.Module):
         # self.matrix = Variable(torch.Tensor(self.matrix_size, D))
         # torch.nn.init.xavier_uniform(self.matrix, gain=1)
         #
-        # self.r = nn.Linear(2*len(Ks) * Co,len(Ks) * Co)
-        # self.z = nn.Linear(2* len(Ks) * Co, len(Ks) * Co)
-        # self._h = nn.Linear(2* len(Ks) * Co, len(Ks) * Co)
+        self.r = nn.Linear(2*len(Ks) * Co,len(Ks) * Co)
+        self.z = nn.Linear(2* len(Ks) * Co, len(Ks) * Co)
+        self._h = nn.Linear(2* len(Ks) * Co, len(Ks) * Co)
         self.mix_2 = []
         for _ in range(self.m):
             self.mix_2.append(nn.Linear(int((D+len(Ks) * Co)/self.m),int((len(Ks) * Co)/self.m)).cuda())
@@ -57,31 +57,28 @@ class CNN_Gate_Aspect_Text(nn.Module):
         aspect_v = self.aspect_embed(aspect)  # (N, L', D)
         aspect_v = aspect_v.sum(1) / aspect_v.size(1)
 
-        x = [F.tanh(conv(feature.transpose(1, 2))) for conv in self.convs1]  # [(N,Co,L), ...]*len(Ks)
-        y = [F.relu(conv(feature.transpose(1, 2)) + self.fc_aspect(aspect_v).unsqueeze(2)) for conv in self.convs2]
-        x = [i * j for i, j in zip(x, y)]
-
+        x = [F.relu(conv(feature.transpose(1, 2))) for conv in self.convs1]  # [(N,Co,L), ...]*len(Ks)
+        y = [self.fc_aspect(aspect_v).unsqueeze(2) for conv in self.convs2]
+        # x = [i * j for i, j in zip(x, y)]
         # pooling method
-        # x0 = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]  # [(N,Co), ...]*len(Ks)
-        # x0 = [i.view(i.size(0), -1) for i in x0]
-        # y0 = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in y]  # [(N,Co), ...]*len(Ks)
-        # y0 = [i.view(i.size(0), -1) for i in y0]
-        # x0 = torch.cat(x0, 1)
-        # y0 = torch.cat(y0, 1)
-        # r = self.r(torch.cat([x0,y0],1))
-        # r = F.tanh(r)
-        # z = self.z(torch.cat([x0, y0], 1))
-        # z = F.tanh(z)
-        # _h = self._h(torch.cat([r*x0, y0], 1))
-        # new_h = (1 - z) * x0 + z * _h
-
         x0 = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]  # [(N,Co), ...]*len(Ks)
         x0 = [i.view(i.size(0), -1) for i in x0]
-
+        y0 = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in y]  # [(N,Co), ...]*len(Ks)
+        y0 = [i.view(i.size(0), -1) for i in y0]
         x0 = torch.cat(x0, 1)
-        # x0 = torch.cat([x0,aspect_v],1)
-        # x0 = self.aspect_re(torch.cat([x0,aspect_v],1))
-        # x0 = F.relu(x0)
+        y0 = torch.cat(y0, 1)
+        r = self.r(torch.cat([x0,y0],1))
+        r = F.sigmoid(r)
+        z = self.z(torch.cat([x0, y0], 1))
+        z = F.sigmoid(z)
+        _h = self._h(torch.cat([r*x0, y0], 1))
+        x0 = (1 - z) * x0 + z * _h
+
+        # x0 = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]  # [(N,Co), ...]*len(Ks)
+        # x0 = [i.view(i.size(0), -1) for i in x0]
+        #
+        # x0 = torch.cat(x0, 1)
+
         x0 = F.dropout(x0)
         logit = self.fc1(x0)  # (N,C)
         length = x0.size(1)//self.decoder_num
