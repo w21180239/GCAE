472e616af204694e8b877ed5ff2c21ef9c8d0ca6
wardliu <w21180239@163.com>
2019-05-10 11:13:55 +0800

    bagging_training

--- model_files/cnn_train.py
+++ model_files/cnn_train.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 import torch.nn as nn
 from torch.utils.data import DataLoader,TensorDataset
 import numpy as np
-
+from attentionbagging import AttentionBagging
 
 def bagging_train(train_iter,dev_iter,model,args):
     time_stamps = []
@@ -16,16 +16,16 @@ def bagging_train(train_iter,dev_iter,model,args):
     model.train()
     start_time = time.time()
     dev_acc, mixed_acc = 0, 0
-    for i in range(model.num):
-        for epoch in range(1, args.epoch+1):
+    for i in range(model.bagging_num):
+        for epoch in range(1, args.epochs+1):
             for _,batch in enumerate(train_iter):
                 feature, target = batch
-                feature,target = torch.Tensor(feature),torch.Tensor(target)
-
+                if len(feature) < 2:
+                    continue
                 if args.cuda:
                     feature, target = feature.cuda(), target.cuda()
 
-                logit,bagging = model(feature)
+                bagging,logit = model(feature)
 
                 out = bagging[i]
                 loss = F.cross_entropy(out, target)
@@ -37,7 +37,7 @@ def bagging_train(train_iter,dev_iter,model,args):
                 steps += 1
                 if steps % args.log_interval == 0:
                     corrects = (torch.max(logit, 1)[1].view(target.size()).data == target.data).sum()
-                    accuracy = 100.0 * corrects/batch.batch_size
+                    accuracy = 100.0 * corrects/target.size(0)
                     if args.verbose == 1:
                         sys.stdout.write(
                             '\rEpoch: {}  Batch[{}] - loss: {:.6f}  acc: {:.4f}%'.format(epoch,steps,
@@ -52,7 +52,7 @@ def bagging_train(train_iter,dev_iter,model,args):
                     save_path = '{}_steps{}.pt'.format(save_prefix, steps)
                     torch.save(model, save_path)
 
-            if epoch == args.epoch:
+            if epoch == args.epochs:
                 dev_acc = bagging_eval(dev_iter, model,i, args)
 
 
@@ -67,12 +67,12 @@ def bagging_eval(data_iter, model,num, args):
 
     for _,batch in enumerate(data_iter):
         feature, target = batch
-        feature, target = torch.Tensor(feature), torch.Tensor(target)
-
+        if len(feature) < 2:
+            continue
         if args.cuda:
             feature, target = feature.cuda(), target.cuda()
 
-        final_out,bagging = model(feature)
+        bagging,final_out = model(feature)
 
         final_loss = F.cross_entropy(final_out, target, size_average=False)
         final_avg_loss += final_loss.data
@@ -104,7 +104,7 @@ def generate_bagging_iter(train_iter, dev_iter,model, args):
     dev_tar = []
     for batch in train_iter:
         feature, aspect, target = batch.text, batch.aspect, batch.sentiment
-        train_tar.append(target)
+
         feature.data.t_()
         if len(feature) < 2:
             continue
@@ -112,7 +112,7 @@ def generate_bagging_iter(train_iter, dev_iter,model, args):
             aspect.data.unsqueeze_(0)
         aspect.data.t_()
         target.data.sub_(1)  # batch first, index align
-
+        train_tar.append(target)
         if args.cuda:
             feature, aspect, target, = feature.cuda(), aspect.cuda(), target.cuda()
 
@@ -120,7 +120,6 @@ def generate_bagging_iter(train_iter, dev_iter,model, args):
         train.append(bagging_input)
     for batch in dev_iter:
         feature, aspect, target = batch.text, batch.aspect, batch.sentiment
-        dev_tar.append(target)
         feature.data.t_()
         if len(feature) < 2:
             continue
@@ -128,16 +127,21 @@ def generate_bagging_iter(train_iter, dev_iter,model, args):
             aspect.data.unsqueeze_(0)
         aspect.data.t_()
         target.data.sub_(1)  # batch first, index align
+        dev_tar.append(target)
 
         if args.cuda:
             feature, aspect, target, = feature.cuda(), aspect.cuda(), target.cuda()
 
         _, bagging_input = model(feature, aspect)
         dev.append(bagging_input)
+    train = torch.cat(train,0)
+    train_tar = torch.cat(train_tar,0)
+    dev=dev[0]
+    dev_tar = dev_tar[0]
     train_dataset = TensorDataset(train,train_tar)
     dev_dataset = TensorDataset(dev, dev_tar)
-    train_loader = DataLoader(train_dataset,128,True)
-    dev_loader = DataLoader(dev_dataset, 128, True)
+    train_loader = DataLoader(train_dataset,256,True)
+    dev_loader = DataLoader(dev_dataset, 256, True)
     return train_loader,dev_loader
 
 
@@ -150,9 +154,7 @@ def train(train_iter, dev_iter, mixed_test_iter, model, args, text_field, aspect
     model.train()
     start_time = time.time()
     dev_acc, mixed_acc = 0, 0
-    for name, p in model.named_parameters():
-        if name.split('.')[0]=='attention_bagging':
-            p.requires_grad = False
+
     for epoch in range(1, args.epochs+1):
         for batch in train_iter:
             feature, aspect, target = batch.text, batch.aspect, batch.sentiment
@@ -169,7 +171,7 @@ def train(train_iter, dev_iter, mixed_test_iter, model, args, text_field, aspect
                 feature, aspect, target, = feature.cuda(), aspect.cuda(), target.cuda()
 
             optimizer.zero_grad()
-            logit, _, _, _,_ = model(feature, aspect)
+            logit,_ = model(feature, aspect)
 
             loss = F.cross_entropy(logit, target)
             loss.backward(retain_graph=True)
@@ -198,7 +200,7 @@ def train(train_iter, dev_iter, mixed_test_iter, model, args, text_field, aspect
                 torch.save(model, save_path)
 
         if epoch == args.epochs:
-            dev_acc, _, _ = eval(dev_iter, model, args)
+            dev_acc = eval(dev_iter, model, args)
             # if mixed_test_iter:
             #     mixed_acc, _, _ = eval(mixed_test_iter, model, args)
             # else:
@@ -209,62 +211,12 @@ def train(train_iter, dev_iter, mixed_test_iter, model, args, text_field, aspect
                 # print('\n{:.4f} - {:.4f} - {:.4f}'.format(dev_acc, mixed_acc, delta_time))
                 time_stamps.append((dev_acc, delta_time))
                 # print()
-    for name, p in model.named_parameters():
-        if name.split('.')[0] != 'attention_bagging':
-            p.requires_grad = False
-        else:
-            p.requires_grad = True
-    for ii in range(10):
-        for epoch in range(1, 3):
-            for batch in train_iter:
-                feature, aspect, target = batch.text, batch.aspect, batch.sentiment
+    train_loader, dev_loader = generate_bagging_iter(train_iter,dev_iter,model,args)
+    bagging_model = AttentionBagging(300,4,100,0.5,True)
+    if args.cuda:
+        bagging_model.cuda()
+    bagging_train(train_loader,dev_loader,bagging_model,args)
 
-                feature.data.t_()
-                if len(feature) < 2:
-                    continue
-                if not args.aspect_phrase:
-                    aspect.data.unsqueeze_(0)
-                aspect.data.t_()
-                target.data.sub_(1)  # batch first, index align
-
-                if args.cuda:
-                    feature, aspect, target = feature.cuda(), aspect.cuda(), target.cuda()
-
-                optimizer.zero_grad()
-                _, _, _, logit,bagging = model(feature, aspect)
-
-                out = bagging[ii]
-                loss = F.cross_entropy(out, target)
-                loss.backward(retain_graph=True)
-                optimizer.step()
-                optimizer.zero_grad()
-
-
-                steps += 1
-                if steps % args.log_interval == 0:
-                    corrects = (torch.max(logit, 1)[1].view(target.size()).data == target.data).sum()
-                    accuracy = 100.0 * corrects/batch.batch_size
-                    if args.verbose == 1:
-                        sys.stdout.write(
-                            '\rEpoch: {}  Batch[{}] - loss: {:.6f}  acc: {:.4f}%'.format(epoch,steps,
-                                                                                     loss.data,
-                                                                                     accuracy,
-                                                                                     ))
-
-
-                if steps % args.save_interval == 0:
-                    if not os.path.isdir(args.save_dir): os.makedirs(args.save_dir)
-                    save_prefix = os.path.join(args.save_dir, 'snapshot')
-                    save_path = '{}_steps{}.pt'.format(save_prefix, steps)
-                    torch.save(model, save_path)
-
-            if epoch == 2:
-                dev_acc, _, _ = eval(dev_iter, model, args)
-
-
-            if args.verbose == 1:
-                delta_time = time.time() - start_time
-                time_stamps.append((dev_acc, delta_time))
     return (dev_acc, mixed_acc), time_stamps
 
 
@@ -286,25 +238,19 @@ def eval(data_iter, model, args):
         if args.cuda:
             feature, aspect, target = feature.cuda(), aspect.cuda(), target.cuda()
 
-        logit, pooling_input, relu_weights,bagging_out,bagging = model(feature, aspect)
+        logit,_ = model(feature, aspect)
         loss = F.cross_entropy(logit, target, size_average=False)
-        bagging_loss = F.cross_entropy(bagging_out, target, size_average=False)
         avg_loss += loss.data
         corrects += (torch.max(logit, 1)
                      [1].view(target.size()).data == target.data).sum()
-        bagging_avg_loss += bagging_loss.data
-        bagging_corrects += (torch.max(bagging_out, 1)
-                     [1].view(target.size()).data == target.data).sum()
+
 
     size = len(data_iter.dataset)
     avg_loss = loss.data/size
-    bagging_avg_loss = bagging_loss.data/size
     xx= int(corrects.cpu().numpy())
     accuracy = 100.0 * xx/size
-    xx = int(bagging_corrects.cpu().numpy())
-    bagging_accuracy = 100.0 * xx/size
     model.train()
     if args.verbose:
-        print('\nEvaluation - loss: {:.6f}  acc: {:.4f}%({}/{})  bagging_loss: {:.6f}  bagging_acc: {:.4f}%({}/{})'.format(
-           avg_loss, accuracy, corrects, size,bagging_avg_loss, bagging_accuracy, bagging_corrects, size))
-    return accuracy, pooling_input, relu_weights
+        print('\nEvaluation - loss: {:.6f}  acc: {:.4f}%({}/{})'.format(
+           avg_loss, accuracy, corrects, size,bagging_avg_loss))
+    return accuracy
